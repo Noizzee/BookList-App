75f5abab9ffcac58686b9e64dffb0e2f77f80468
Noizzee <37045144+Noizzee@users.noreply.github.com>
2019-01-16 14:00:00 +0100

    added alerts

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ class UI {
         isbn: "45545"
       }
     ];
-
+    
     const books = StoredBooks;
     books.forEach(book => UI.addBookToList(book)); //Loops true the array and runs the addBookToList function for each element
   }
-
+  //Add a book to the list Method
   static addBookToList(book) {
     const list = document.querySelector('#book-list');
     const row = document.createElement('tr');
@@ -38,6 +38,29 @@ class UI {
     `;
     list.appendChild(row);
   }
+  //Delete a book Method
+  static deleteBook(element) {
+    //Checks if the classlist of the clicked element contains the class "delete" which is the button
+    if (element.classList.contains("delete")) {
+      //Removes the parentElement of the parentElement which is the TR
+      element.parentElement.parentElement.remove();
+    }
+  }
+  //Show Alert Method
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+  }
+  //Clear the inputfields Method
+  static clearFields() {
+    document.querySelector('#title').value = "";
+    document.querySelector('#author').value = "";
+    document.querySelector('#isbn').value = "";
+  }
 }
 
 //Store Class: Handles Storage
@@ -45,7 +68,29 @@ class UI {
 //Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-
 //Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  //Prevent Default Action
+  e.preventDefault();
+  //Get Input Values
+  let title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const isbn = document.querySelector('#isbn').value;
+  
+  //Validate user input
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert('Please fill in all fields', 'danger');
+  } else {
+    //Instansiate a New Book
+    const book = new Book (title, author, isbn);
+    //Add Book to the UI
+    UI.addBookToList(book);
+    //Clear the inputfields
+    UI.clearFields();
+  }
+});
 
 //Event: Remove a Book
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  UI.deleteBook(e.target);
+});
